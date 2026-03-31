7edf6afc3885dd6af1900b460d38143e4104ba1f
shiba <shiba.spj@hotmail.com>
2026-03-31 23:19:42 +0530

    v1

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app.js';
 import { logger } from './utils/logger.js';
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 5000;
 const HOST = process.env.HOST || '0.0.0.0';
 
 const start = async () => {
